af44bb646888186e223e00ad5d932d2563dea9e0
heguangyu5 <heguangyu5@qq.com>
2020-07-21 18:00:41 +0800

    mysql-3307-in-memory

--- ourblog/tests/bootstrap.php
+++ ourblog/tests/bootstrap.php
@@ -5,16 +5,14 @@ include __DIR__ . '/../autoload.php';
 
 // OurBlog_Db
 putenv('DB_HOST=127.0.0.1');
-putenv('DB_PORT=3306');
+putenv('DB_PORT=3307');
 putenv('DB_DATABASE=our_blog_test');
-putenv('DB_USER=ourbloguser');
-putenv('DB_PASSWORD=ourblogpasswd');
+putenv('DB_USER=root');
+putenv('DB_PASSWORD=123456');
 
 include 'PHPUnitNoNamespace.php';
 
 abstract class OurBlog_DatabaseTestCase extends PHPUnit_DbUnit_Mysql_TestCase
 {
-    protected static $mysqlPort     = 3306;
-    protected static $mysqlDbname   = 'our_blog_test';
-    protected static $mysqlUsername = 'rootpw';
+    protected static $mysqlDbname = 'our_blog_test';
 }
--- ourblog/tests/sync-test-db.sh
+++ ourblog/tests/sync-test-db.sh
@@ -3,6 +3,6 @@
 echo "dump..."
 sudo mysqldump -d our_blog > /tmp/our_blog.sql
 echo "import..."
-sudo mysql -e "DROP DATABASE IF EXISTS our_blog_test;CREATE DATABASE our_blog_test DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
-sudo mysql our_blog_test < /tmp/our_blog.sql
+mysql -h127.0.0.1 -P3307 -uroot -p123456 -e "DROP DATABASE IF EXISTS our_blog_test;CREATE DATABASE our_blog_test DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+mysql -h127.0.0.1 -P3307 -uroot -p123456 our_blog_test < /tmp/our_blog.sql
 echo "done"
