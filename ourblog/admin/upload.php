eae4177d749abad44b44f0a85f71cf645eccddbf
heguangyu5 <heguangyu5@qq.com>
2020-07-21 12:59:31 +0800

    chapter17 finished

---
+++ ourblog/admin/upload.php
@@ -0,0 +1,26 @@
+<?php
+
+include __DIR__ . '/check-login.php';
+include __DIR__ . '/../autoload.php';
+
+$error = '';
+if ($_FILES) {
+    try {
+        $upload = new OurBlog_Upload(__DIR__ . '/../upload', $_SESSION['id']);
+        $filename = $upload->upload();
+        echo "Upload success! file saved as $filename.";
+        exit;
+    } catch (InvalidArgumentException $e) {
+        die($e->getMessage());
+    } catch (Exception $e) {
+        die('SERVER ERROR');
+    }
+}
+
+?>
+
+<h2>Upload File</h2>
+<form enctype="multipart/form-data" method="POST">
+    <input name="file" type="file">
+    <input type="submit" value="Upload">
+</form>
